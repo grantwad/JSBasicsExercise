d8e782fb833b2c64ae5138d1751463468fd91a14
Grant <grant.c.wadlington@gmail.com>
2021-10-06 11:29:11 -0400

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,26 @@
+// JS BASICS EXERCISE
+// 1
+console.log(14);
+ 
+// 2
+// Numbers, Strings, Booleans, Null, & Undefined
+ 
+// 3
+// Addition is +, Subtraction is -, Multiplication *, Division is /, Exponentiation is **, Modulus is %
+ 
+// 4
+console.log(5%4);
+console.log(5%2);
+// console.log(1%5);
+ 
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+ 
+// 6
+console.log("Hawk" + "eye");
+ 
+// 7
+console.log("Matt".length + 'Riley'.length);
+ 
+// 8
+console.log("Avengers: Endgame"[16]);
